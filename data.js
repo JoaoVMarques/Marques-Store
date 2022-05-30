5cdc9b5f55434f20f67a299b2239d6d47a292ccb
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-05-30 19:27:34 -0300

    Adicionando produtos dinamicamente

--- data.js
+++ data.js
@@ -1,5 +1,26 @@
-const Items = [
-  { Src: '/items/fone.png',
+const items = [
+  { 
+  src: '/items/fone.png',
   title: 'Fone HyperX Cloud Alpha',
-  price: 'R$499,90'}
+  price: 'R$499,90',
+  size: 'normal'
+},
+  {
+  src: '/items/alloy-fps.png',
+  title: 'Teclado HyperX Alloy FPS',
+  price: 'R$489,00',
+  size: 'big'
+},
+  { 
+  src: '/items/pulsefireFps.png',
+  title: 'Mouse Hyperx Pulsefire FPS',
+  price: 'R$159,99',
+  size: 'big'
+},
+  { 
+  src: '/items/memoriaRam.png',
+  title: 'Memória HyperX Predator de 8GB',
+  price: 'R$320,90',
+  size: 'big'
+},
 ]
---
+++ script.js
@@ -0,0 +1,40 @@
+function createButton() {
+  const button = document.createElement('button');
+  button.textContent = 'Comprar';
+  button.classList.add('item-button');
+  return button;
+}
+
+function createTexts(classText, itemText) {
+  const text = document.createElement('p');
+  text.classList.add(classText);
+  text.textContent = itemText;
+  return text;
+}
+
+function createDisplayChilds(item) {
+  const figure = document.createElement('figure');
+  const img = document.createElement('img');
+  img.setAttribute('src', item.src);
+  img.classList.add('figure-img', item.size);
+  figure.appendChild(img);
+  return figure;
+}
+
+function ShowItemsInCard(item) {
+  const itemCard = document.createElement('div');
+  itemCard.classList.add('item-Card');
+  itemCard.appendChild(createDisplayChilds(item));
+  itemCard.appendChild(createTexts('item-title', item.title));
+  itemCard.appendChild(createTexts('item-price', item.price));
+  itemCard.appendChild(createButton());
+  return itemCard;
+}
+
+
+window.onload = () => {
+  const products = document.getElementsByClassName('items-products')[0];
+  items.forEach(element => {
+    products.appendChild(ShowItemsInCard(element));
+  });
+}
