053384d8a9cc4a8da5ab4abbaae694beac64e0b2
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-06-08 17:41:31 -0300

    Arrumando erro das imagens nao aparecendo

--- data.js
+++ data.js
@@ -1,48 +1,48 @@
 const items = [
   { 
-  src: '/items/fone.png',
+  src: './items/fone.png',
   title: 'Fone HyperX Cloud Alpha',
   price: 'R$499,90',
   size: 'normal'
 },
   {
-  src: '/items/alloy-fps.png',
+  src: './items/alloy-fps.png',
   title: 'Teclado HyperX Alloy FPS',
   price: 'R$489,00',
   size: 'big'
 },
   { 
-  src: '/items/pulsefireFps.png',
+  src: './items/pulsefireFps.png',
   title: 'Mouse Hyperx Pulsefire FPS',
   price: 'R$159,99',
   size: 'big'
 },
   { 
-  src: '/items/memoriaRam.png',
+  src: './items/memoriaRam.png',
   title: 'Memória HyperX Predator 8GB',
   price: 'R$320,90',
   size: 'big'
 },
   { 
-  src: '/items/cloudRevolver.png',
+  src: './items/cloudRevolver.png',
   title: 'Fone HyperX Cloud Revolver',
   price: 'R$589,90',
   size: 'big'
 },
 { 
-  src: '/items/mousepadFury.png',
+  src: './items/mousepadFury.png',
   title: 'Mouse Pad Fury',
   price: 'R$159,00',
   size: 'big'
 },
 { 
-  src: '/items/pulsefire.jpg',
+  src: './items/pulsefire.jpg',
   title: 'Mouse Pulsefire Haste',
   price: 'R$209,90',
   size: 'big'
 },
 { 
-  src: '/items/alloyElite.png',
+  src: './items/alloyElite.png',
   title: 'Teclado HyperX Alloy Elite',
   price: 'R$1.012,73',
   size: 'big'
